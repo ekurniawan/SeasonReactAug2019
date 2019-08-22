09a41b110ff6af260340041a00fbfff5f73f7b60
Erick Kurniawan <erick.kurniawan@gmail.com>
2019-08-22 14:42:42 +0700

    react component

--- src/components/App.js
+++ src/components/App.js
@@ -2,7 +2,20 @@ import React from 'react';
 
 class App extends React.Component{
     constructor(props){
-        
+        super(props);
+        this.state = {lat:null};
+        window.navigator.geolocation.getCurrentPosition(
+            pos=>{
+                this.setState({lat:pos.coords.latitude});
+            },
+            err =>{
+                console.log(err);
+            }
+        );
+    }
+
+    render(){
+        return (<div>Latitude: {this.state.lat}</div>);
     }
 }
 
